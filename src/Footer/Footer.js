f0d032c429a5044031663fa26d7657bd1770cb0c
Leonmisha <48850081+Leonmisha@users.noreply.github.com>
2020-04-04 19:28:34 +0300

    Update Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -3,9 +3,19 @@ import React from 'react'
 class Footer extends React.Component{
   render () {
     return (
-      <div id="Footer">
-        
-      </div>
+      <footer id="Footer">
+        <div class="copyright">&copy; 2020 Roadmap4devs</div>
+        <div class="rel-sites">
+          <span class="rel-sites-title">
+            Так же вас может заинтересовать:
+          </span>
+          <div class="rel-sites-list">
+            <a href="https://www.freecodecamp.org/">FreeCodeCamp</a>
+            <a href="https://www.codewars.com/">Codewars</a>
+            <a href="https://www.coursera.org/browse/computer-science">Coursera</a>
+          </div>
+        </div>
+      </footer>
     )
   }
 }
