34ddb1c4b24651867e8f3a92184c7056f354a5c2
HermesC <rybakirill01@gmail.com>
2020-05-08 20:13:13 +0300

    +ApplicationProg,WebProg

--- src/components/Content/Main/ApplicationProg/ApplicationProg.js
+++ src/components/Content/Main/ApplicationProg/ApplicationProg.js
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import React from 'react'
+import Tile from './Tile/Tile.js'
 
-class Main extends React.Component {
+class ApplicationProg extends React.Component {
   render () {
     return (
-
+        <div id="ApplicationProg">
+          <Tile proftitle="Игры" profdesc="Популярное и высокооплачеваемое направлением в IT индустрии"/>
+          <Tile proftitle="Десктопная разработка" profdesc="Довольно востребованное направление, имеет отличный порог входа"/>
+          <Tile proftitle="Мобильная разработка" profdesc="Отличается высокой универсальностю и стандартизированностью, отличный порог входа"/>
+        </div>
     )
   }
 }
-export default Main
+export default ApplicationProg
--- src/components/Content/Main/WebProg/WebProg.js
+++ src/components/Content/Main/WebProg/WebProg.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import React from 'react'
+import Tile from './Tile/Tile.js'
 
-class Main extends React.Component {
+class WebProg extends React.Component {
   render () {
     return (
-
+      <div id="WebProg">
+        <Tile proftitle="Front-end" profdesc="Отличный порог входа, множество курсов и учебных материалов, большое количество вакантных мест"/>
+        <Tile proftitle="Back-end" profdesc="Довольно востребованное направление, отличается высокой универсальностю, хотя порог вхождения может быть выше, чем у front-end разработчика"/>
+      </div>
     )
   }
 }
-export default Main
+export default WebProg
