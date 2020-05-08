2bca888903d1277f008b131b57b2e48082801939
HermesC <rybakirill01@gmail.com>
2020-05-08 20:54:01 +0300

    Application change

--- src/components/Content/Main/ApplicationProg/ApplicationProg.js
+++ src/components/Content/Main/ApplicationProg/ApplicationProg.js
@@ -1,15 +1,22 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import React from 'react'
 import Tile from './Tile/Tile.js'
+import
 
 class ApplicationProg extends React.Component {
   render () {
     return (
-        <div id="ApplicationProg">
-          <Tile proftitle="Игры" profdesc="Популярное и высокооплачеваемое направлением в IT индустрии"/>
-          <Tile proftitle="Десктопная разработка" profdesc="Довольно востребованное направление, имеет отличный порог входа"/>
-          <Tile proftitle="Мобильная разработка" profdesc="Отличается высокой универсальностю и стандартизированностью, отличный порог входа"/>
-        </div>
+        <Router id="ApplicationProg">
+          <Link to={'/application-prog/games'}><Tile proftitle="Игры" profdesc="Популярное и высокооплачеваемое направлением в IT индустрии"/></Link>
+          <Link to={'/application-prog/desktop'}><Tile proftitle="Десктопная разработка" profdesc="Довольно востребованное направление, имеет отличный порог входа"/></Link>
+          <Link to={'/application-prog/mobile'}><Tile proftitle="Мобильная разработка" profdesc="Отличается высокой универсальностю и стандартизированностью, отличный порог входа"/></Link>
+
+          <Switch>
+            <Route path={'/application-prog/games'}>application-prog/games</Route>
+            <Route path={'/application-prog/desktop'}>application-prog/desktop</Route>
+            <Route path={'/application-prog/mobile'}>application-prog/mobile</Route>
+          </Switch>
+        </Router>
     )
   }
 }
--- src/components/Content/Main/Main.js
+++ src/components/Content/Main/Main.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import './Main.css'
-import './Tile/Tile.js'
-import ''
+import Tile './Tile/Tile.js'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 class Main extends React.Component {
   render () {
     return (
       <Router>
-          // <div id="Main">
-          <Link to={'/system-prog'}><Tile proftitle='Title' profdesc='Description' profimg='path'/></Link>
-          <Link to={'/application-prog'}><Tile proftitle='Title' profdesc='Description' profimg='path'/></Link>
-          <Link to={'/web-prog'}><Tile proftitle='Title' profdesc='Description' profimg='path'/></Link>
-
-          <Switch className="horizontal-flex">
-            <Route path={'/system-prog'} component={<SystemProg />}/>
-            <Route path={'/application-prog'} component={<ApplicationProg />}/>
-            <Route path={'/web-prog'} component={<WebProg />}/>
+          <Route path="" render={
+            () => {
+              return (
+                <div id="Main" className="horizontal-flex">
+                  <Link to={'/system-prog'}><Tile proftitle='Title' profdesc='Description' profimg='path'/></Link>
+                  <Link to={'/application-prog'}><Tile proftitle='Title' profdesc='Description' profimg='path'/></Link>
+                  <Link to={'/web-prog'}><Tile proftitle='Title' profdesc='Description' profimg='path'/></Link>
+                </div>
+          )
+          }
+          }/>
+          <Switch>
+            <Route path={'/system-prog'} component={SystemProg}/>
+            <Route path={'/application-prog'} component={ApplicationProg}/>
+            <Route path={'/web-prog'} component={WebProg}/>
           </Switch>
-          // </div>
       </Router>
     )
   }
