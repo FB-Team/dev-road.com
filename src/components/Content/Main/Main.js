d0177df68d1b4822be04fb244503f0e2fc1a8991
HermesC <rybakirill01@gmail.com>
2020-05-08 19:51:00 +0300

    Working on tiles...

--- src/components/Content/Main/Main.js
+++ src/components/Content/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Main.css'
 import './Tile/Tile.js'
+import ''
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 class Main extends React.Component {
@@ -10,10 +11,12 @@ class Main extends React.Component {
           // <div id="Main">
           <Link to={'/system-prog'}><Tile proftitle='Title' profdesc='Description' profimg='path'/></Link>
           <Link to={'/application-prog'}><Tile proftitle='Title' profdesc='Description' profimg='path'/></Link>
-          
-          <Switch>
-            <Route path={'/system-prog'} component={}/>
-            <Route path={'/application-prog'} component={}/>
+          <Link to={'/web-prog'}><Tile proftitle='Title' profdesc='Description' profimg='path'/></Link>
+
+          <Switch className="horizontal-flex">
+            <Route path={'/system-prog'} component={<SystemProg />}/>
+            <Route path={'/application-prog'} component={<ApplicationProg />}/>
+            <Route path={'/web-prog'} component={<WebProg />}/>
           </Switch>
           // </div>
       </Router>
---
+++ src/components/Content/Main/WebProg/WebProg.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import React from 'react'
+
+class Main extends React.Component {
+  render () {
+    return (
+
+    )
+  }
+}
+export default Main
