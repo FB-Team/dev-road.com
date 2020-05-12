90a62eff6ac080f91512c8170c4b9ff932801044
Leonmisha <48850081+Leonmisha@users.noreply.github.com>
2020-05-12 12:45:54 +0300

    Update Main.js

--- src/components/Content/Main/Main.js
+++ src/components/Content/Main/Main.js
@@ -5,25 +5,21 @@ import SystemProg from './SystemProg/SystemProg.js'
 import ApplicationProg from './ApplicationProg/ApplicationProg.js'
 import StartProg from './StartProg/StartProg.js'
 import WebProg from './WebProg/WebProg.js'
-import {createBrowserHistory} from 'history'
+//import {createBrowserHistory} from 'history'
 class Main extends React.Component {
-  constructor(props){
-    super(props);
-    this.history = createBrowserHistory();
-  }
+  back(e) {
+    e.stopPropagation();
+    window.history.back();
+  };
   render () {
     return (
-      <div>
-        {/*<input type="button" onClick={()=> this.history.back()} value="Назад"></input>*/}
-
       <Router history={this.history}>
+          <input type="button" onClick={this.back} value="Назад"></input>
           <Route exact path={'/'} component={StartProg}/>
           <Route path={'/system-prog'} component={SystemProg}/>
           <Route path={'/application-prog'} component={ApplicationProg}/>
           <Route path={'/web-prog'} component={WebProg}/>
-
       </Router>
-      </div>
     )
   }
 }
