fe91c1d0067143f5da7df7c125341b205063b946
HermesC <rybakirill01@gmail.com>
2020-05-13 18:01:03 +0300

    ApplicationReduxProg

---
+++ src/components/Content/Main/MainDataRedux/ApplicationProgRedux/Desktop/CPlusQt/CPlusQt.js
@@ -0,0 +1,13 @@
+const CPlusQt = {
+  meta: {
+    hasChildren: true,
+    path:        '/application-prog/desktop/cplusqt'
+  },
+  data:{
+    profdesc: 'Десктопная разработка с использованием С++ и библиотеки Qt',
+    proftitle: 'C++',
+  },
+  children: [
+  ]
+}
+export default CPlusQt
