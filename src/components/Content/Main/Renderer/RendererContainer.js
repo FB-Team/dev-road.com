741f21b9be5aefe6991f7e99d5992643385239c3
HermesC <rybakirill01@gmail.com>
2020-05-15 18:59:48 +0300

    MINOR CHANGES

--- src/components/Content/Main/Renderer/RendererContainer.js
+++ src/components/Content/Main/Renderer/RendererContainer.js
@@ -4,13 +4,16 @@ import RenderExpanded from './RendererExpanded'
 import {Route} from 'react-router'
 
 function RendererContainer(props) {
-
-  let ResultingComponent = props.toRender.meta.hasChildren
-    ? RenderTiles
-    : RenderExpanded
-
-  return (<div className={props.toRender.meta.id}>
-    <Route render={match => <ResultingComponent pathname={match.location.pathname} component={props.toRender}/>}/>
-  </div>)
+  return (
+    <div className={props.toRender.meta.id}>
+        <Route render={match => {
+            let ResultingComponent = props.toRedner.meta.hasChildren
+              ? RenderTiles
+              : RenderExpanded;
+          return <ResultingComponent component={this.props.toRender}/>
+        }
+      }
+  </div>
+  )
 }
 export default RendererContainer
