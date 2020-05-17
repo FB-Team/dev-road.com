40fc0b1fe93979893b93eb9143d633df64368546
Leonmisha <48850081+Leonmisha@users.noreply.github.com>
2020-05-18 00:08:42 +0300

    test atom commit+push

--- src/components/Content/Main/Renderer/RenderTiles/RenderTiles.js
+++ src/components/Content/Main/Renderer/RenderTiles/RenderTiles.js
@@ -9,7 +9,10 @@ const RenderTiles = props => {
     console.log('Onclick entered')
     props.transfer(event.currentTarget.id)
   }
-let NavTiles = props.component.children.map(child => <div className={s.NavTile} id={child.meta.id} onClick={onClickTile}><NavTile to={child.meta.path} onClick={onClickTile}proftitle={child.data.proftitle} profdesc={child.data.profdesc}/></div>
+let NavTiles = props.component.children
+  .map(child => <div className={s.NavTile} id={child.meta.id} 
+    onClick={onClickTile}><NavTile to={child.meta.path} onClick={onClickTile}
+    proftitle={child.data.proftitle} profdesc={child.data.profdesc}/></div>
 )
   return (
 <div id='rendererTiles' className="horizontal-flex">{NavTiles}</div>
