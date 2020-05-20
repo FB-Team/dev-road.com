39980d33bc0dc3caf1672b1f664c3da8c9a7112f
HermesC <rybakirill01@gmail.com>
2020-05-20 21:12:33 +0300

    Revert "minor style changes"
    This reverts commit f2fcd652d7982eb9a7dbb3be2a53e518bcc5ccc2.

--- src/components/Content/Main/Main.js
+++ src/components/Content/Main/Main.js
@@ -1,10 +1,15 @@
 import {connect} from 'react-redux'
+
+
 import {loadPage,transfer,jump} from '../../../Redux/actions.js'
 import RendererContainer from './Renderer/RendererContainer';
+
 
 function mapStateToProps(state){
   return{
     toRender: state.toRender
   }
 }
+
+
 export default connect(mapStateToProps, {loadPage, transfer, jump})(RendererContainer)
