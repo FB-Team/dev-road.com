f2fcd652d7982eb9a7dbb3be2a53e518bcc5ccc2
HermesC <rybakirill01@gmail.com>
2020-05-20 20:34:22 +0300

    minor style changes

--- src/components/Content/Main/Main.js
+++ src/components/Content/Main/Main.js
@@ -1,15 +1,10 @@
 import {connect} from 'react-redux'
-
-
 import {loadPage,transfer,jump} from '../../../Redux/actions.js'
 import RendererContainer from './Renderer/RendererContainer';
-
 
 function mapStateToProps(state){
   return{
     toRender: state.toRender
   }
 }
-
-
 export default connect(mapStateToProps, {loadPage, transfer, jump})(RendererContainer)
