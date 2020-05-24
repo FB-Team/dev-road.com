ad0efae8b6b61985a742ae6eb6d3988f865a37f2
Leonmisha <48850081+Leonmisha@users.noreply.github.com>
2020-05-24 12:41:39 +0300

    refactor C# -> C#.net and basic styles to expanded renderer

--- src/components/Content/Main/Renderer/ExpandedRenderer/Introduction/Introduction.js
+++ src/components/Content/Main/Renderer/ExpandedRenderer/Introduction/Introduction.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
 import s from './Introduction.module.css';
+import ReactHtmlParser from 'react-html-parser';
 
  const Introduction = (props) => {
 
+   let description = props.description
+   if (! description.startsWith("<p>")) {description = `<p>${description}</p>`}
+   description = ReactHtmlParser(description)
+
   return (
 <div id='Introduction'>
-    <div className={s.titleWrapper}><span className={s.title}>{props.title}</span></div>
+    <div className={s.title}>{props.title}</div>
     <div className={s.imageWrapper}><img className={s.image} src={props.imgPath} alt="Server error, cannot load an image:-("/></div>
-    <div className={s.descriptionWrapper}><p className={s.description}>{props.description}</p></div>
+    <div className={s.descriptionWrapper}><p className={s.description}>{description}</p></div>
 </div>
   )
 }
