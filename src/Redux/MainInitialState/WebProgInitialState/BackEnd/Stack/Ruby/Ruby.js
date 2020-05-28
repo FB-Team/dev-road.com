ad685f8ea4a08d28f7f41a8c0866f71313c6aa8b
HermesC <rybakirill01@gmail.com>
2020-05-28 23:43:08 +0300

    Update Ruby.js

--- src/Redux/MainInitialState/WebProgInitialState/BackEnd/Stack/Ruby/Ruby.js
+++ src/Redux/MainInitialState/WebProgInitialState/BackEnd/Stack/Ruby/Ruby.js
@@ -7,7 +7,6 @@ import Caching from '../CommonTools/Caching/Caching';
 import DB_Back from '../CommonTools/DB_Back/DB_Back';
 import Internet from '../../../FrontEnd/Theory/Internet/Internet';
 import Security from '../CommonTools/Security/Security';
-import Caching from '../CommonTools/Caching/Caching'
 
 const Ruby = {
   name: 'Ruby(Rails)',
