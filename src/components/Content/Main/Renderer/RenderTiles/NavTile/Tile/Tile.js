39da1233253f4ab1086c848471061b8c2f44ced8
HermesC <rybakirill01@gmail.com>
2020-05-30 22:15:18 +0300

    Animation fix added

--- src/components/Content/Main/Renderer/RenderTiles/NavTile/Tile/Tile.js
+++ src/components/Content/Main/Renderer/RenderTiles/NavTile/Tile/Tile.js
@@ -1,16 +1,39 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
 import s from './Tile.module.css'
+
 const Tile = (props)=>{
 const parsedDelay = props.delay + 's'
-  const animatedDiv = {
-    transitionProperty: "opacity",
-    transition: "1s",
-    transitionDelay:parsedDelay
+  const tileload = keyframes `
+  0%{
+    opacity: 0;
   }
-
-  ;
+  25%{
+    opacity: 0.35;
+  }
+  50%{
+    opacity: 0.7;
+  }
+  75%{
+    opacity: 0.85;
+  }
+  100%{
+    opacity: 1;
+  }
+  `;
+  const Div = styled.div`
+    border-top: none;
+    border-radius: 5px;
+    padding: 0px;
+    height: 100%;
+    width: 400px;
+    margin: 0px;
+    opacity: 0;
+    animation: 1s ease-in ${parsedDelay} ${tileload} ;
+    animation-fill-mode: forwards;
+  `;
     return (
-      <div className={s.Tile} style={animatedDiv}>
+      <Div>
         <div className={s.proftitle}>
           {props.proftitle}
         </div>
@@ -18,7 +41,7 @@ const parsedDelay = props.delay + 's'
           <img src={props.profimg} alt=""/>
         </div>
         <div className={s.profdesc}>{props.profdesc}</div>
-      </div>
+      </Div>
     )
 }
 export default Tile
