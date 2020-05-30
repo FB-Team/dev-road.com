f0b2c27f4cd8fe34c5d46616f94fa6a61193a33b
Leonmisha <48850081+Leonmisha@users.noreply.github.com>
2020-05-30 20:52:15 +0300

    add hover styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
+
+import { Link } from 'react-router-dom' 
 import s from './Header.module.css'
+
 class Header extends React.Component{
   render () {
     return (
       <header className={s.Header}>
-          <span className={s.title}><h1>Roadmap4devs</h1></span>
+          <span className={s.title}><Link to="/"><h1>Roadmap4devs</h1></Link></span>
           {/*<nav className={s.navbar}>
           <a className={s.navlink} href="#">Контакты</a>
           <a className={s.navlink} href="#">Про нас</a>
