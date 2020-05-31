ba56ff340dcc0e4c62ac839d13df4403ff67c781
HermesC <rybakirill01@gmail.com>
2020-05-31 17:16:55 +0300

    Tooltip refactoring

--- src/components/Content/Main/Renderer/RenderTools/RenderTools.js
+++ src/components/Content/Main/Renderer/RenderTools/RenderTools.js
@@ -7,15 +7,18 @@ const RenderTools = (props) => {
   function onclick (event){
     let targetTooltip = event.target.querySelector('div')
     if (targetTooltip){
-      if (currentTooltip)
-        currentTooltip.style.display = 'none'
-        if (currentTooltip != targetTooltip){
+      if (targetTooltip.children.length >= 1){
+        if (currentTooltip)
+          currentTooltip.style.display = 'none'
+        if (targetTooltip === currentTooltip) setTooltip(false)
+        else {
           targetTooltip.style.display = 'flex'
-          targetTooltip.style.left = event.clientX - 20 + 'px'
+          targetTooltip.style.left = event.clientX -20 + 'px'
+          setTooltip(targetTooltip)
         }
-        setTooltip(targetTooltip)
-      }
     }
+   }
+}
   return (
     <div onClick={onclick}>{createTools(props.tool)}</div>
   )
