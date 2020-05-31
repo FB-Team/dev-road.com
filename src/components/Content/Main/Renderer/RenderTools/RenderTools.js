5e1d0fc9f87e141d7631ea61543e8c0aa2e89d9b
HermesC <rybakirill01@gmail.com>
2020-05-31 22:03:11 +0300

    Some changes !!!!!!Need to change Electronics links

--- src/components/Content/Main/Renderer/RenderTools/RenderTools.js
+++ src/components/Content/Main/Renderer/RenderTools/RenderTools.js
@@ -1,16 +1,20 @@
 import React from   'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import createTools from './createTools'
 
 const RenderTools = (props) => {
-  const [currentTooltip, setTooltip] = useState(false)
+  const [currentTooltip, setTooltip] = useState(null)
+  useEffect( () => {
+    if (currentTooltip)
+      currentTooltip.style.display = 'none'
+  }, [props])
   function onclick (event){
     let targetTooltip = event.target.querySelector('div')
     if (targetTooltip){
       if (targetTooltip.children.length >= 1){
         if (currentTooltip)
           currentTooltip.style.display = 'none'
-        if (targetTooltip === currentTooltip) setTooltip(false)
+        if (targetTooltip === currentTooltip) setTooltip(null)
         else {
           targetTooltip.style.display = 'flex'
           targetTooltip.style.left = event.clientX -20 + 'px'
