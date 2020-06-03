2225c02a11611ad927b04d4fa85374b2865bc94f
Leonmisha <48850081+Leonmisha@users.noreply.github.com>
2020-06-03 19:31:14 +0300

    Add react memo to RenderTiles

--- src/components/Content/Main/Renderer/RenderTiles/RenderTiles.js
+++ src/components/Content/Main/Renderer/RenderTiles/RenderTiles.js
@@ -4,20 +4,22 @@ import NavTile from 'components/Common/Tile/NavTile';
 import s from './RenderTiles.module.css'
 
 
-const RenderTiles = props => {
-let delayIncrement = -0.2;
-let NavTiles = props.component.children
-  .map((child, i) => <div key={child.meta.id}
-                     className={s.NavTile}
-                     id={child.meta.id}>
-                  <NavTile delay={delayIncrement += 0.3} to={child.meta.path}
-                           proftitle={child.data.proftitle}
-                           profdesc={child.data.profdesc} profimg={child.data.imgPath}/>
-                </div>
-)
-  return (
-<div id='rendererTiles' className="horizontal-flex">{NavTiles}</div>
+const RenderTiles = React.memo(props => {
+  let delayIncrement = -0.2;
+  let NavTiles = props.component.children
+    .map((child, i) => <div key={child.meta.id}
+                       className={s.NavTile}
+                       id={child.meta.id}>
+                    <NavTile delay={delayIncrement += 0.3}
+                             to={child.meta.path}
+                             proftitle={child.data.proftitle}
+                             profdesc={child.data.profdesc}
+                             profimg={child.data.imgPath}/>
+                  </div>
   )
-}
+    return (
+  <div id='rendererTiles' className="horizontal-flex">{NavTiles}</div>
+    )
+})
 
 export default RenderTiles
