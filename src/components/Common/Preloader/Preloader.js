8ad7ca9490fe659dcc94c075baa7a18435635f47
Leonmisha <48850081+Leonmisha@users.noreply.github.com>
2020-06-08 20:25:43 +0300

    little fixes + prepare svg preloader

--- src/components/Common/Preloader/Preloader.js
+++ src/components/Common/Preloader/Preloader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './Preloader.module.css';
 
 const Preloader = (props) => {
-  return <div class={s.Preloader}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  return <div className={s.Preloader}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
 };
 
 
